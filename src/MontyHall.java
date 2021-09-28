46d538554351d9afe537386bde5b84ba32c2c07a
Darian Martinez <darian.mtz18@gmail.com>
2021-09-28 09:47:28 -0500

    this one should say hello world in my branch

--- src/MontyHall.java
+++ src/MontyHall.java
@@ -1,5 +1,5 @@
 public class MontyHall {
-    static void main(String args[]){
-        System.out.println("hello");
+    public static void main(String args[]){
+        System.out.println("hello world");
     }
 }
